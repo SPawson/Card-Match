d3a32c04fc391a512e68856e764a19bd51502104
Sam Pawson <samuel.pawson@gmail.com>
2019-05-20 12:06:51 +0000

    added card shuffle functionality and start game initialisation

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,48 +12,100 @@ class AudioController {
         startMusic = function() {
                 this.backgroundMusic.play();
         }
-        
+
         stopMusic = function() {
                 this.backgroundMusic.pause();
                 this.backgroundMusic.currentTime = 0;
         }
-        
+
         flip = function() {
                 this.flipSound.play();
         }
-        
+
         match = function() {
                 this.matchSound.play();
         }
-        
+
         success = function() {
                 this.stopMusic();
                 this.succesSound.play();
-                
+
         }
-        
+
 }
 
-   function startMusic() {
-                let audio = new AudioController();
-                audio.startMusic();
+function startMusic() {
+        let audio = new AudioController();
+        audio.startMusic();
+}
+
+class CardMatch {
+        constructor(totalTime, cards) {
+                this.cardArray = cards;
+                this.totalTime = totalTime;
+                this.timeRemaining = totalTime;
+                this.timer = $('#time-remaining');
+                this.turns = $('#turns-count');
+                this.totalClicks = 0
+
+                this.audioController = new AudioController();
         }
 
-$(document).ready(function() {
+        //Randomises the card layout by shuffling the grid order
+        shuffleCards = function(cards) {
+                let randomIndex = 0;
+                let temp = 0;
+                for (let i = this.cardArray.length - 1; i > 0; i--) {
+                        randomIndex = Math.floor(Math.random() * (i + 1));
+                        
+                        
+                      
+                        $(this.cardArray[randomIndex]).css('order', i);
+                        $(this.cardArray[i]).css('order', randomIndex);
+                }
+        }
 
+        startGame = function() {
+                this.cardToBeChecked = null;
+                this.totalClicks = 0;
+                this.timeRemaining = this.totalTime;
+                this.matchedCards = [];
+                this.busy = true;
+
+                this.shuffleCards(this.cardArray);
+        }
+
+
+
+        cardFlip = function(card) {
+                if (this.canCardFlip(card)) {
+                        this.audioController.flip();
+                        this.totalClicks++;
+                        this.turns = this.turns.text(this.totalClicks);
+                        $(card).addClass('visible');
+                }
+        }
+
+        canCardFlip = function(card) {
+                return true;
+        }
+}
+
+$(function() {
         $("#startModal").modal('show');
 
         let cards = $('.card');
+        let game = new CardMatch(100, cards);
+        let start = $('#start-btn')
 
-        //cards.each(card => {
-        //       card.addEventListener('click', () => {
-        //             game.flipCard(card);
-        //   } )
-        //});
+        start.on('click', function() {
+                game.startGame();
 
-     
+        });
 
-
+        cards.on('click', function() {
+                game.cardFlip(this);
+        });
 
 
 });
