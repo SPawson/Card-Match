28cfaf209bce80d6a2b4da8be1e6270d4ba0adfb
Sam Pawson <samuel.pawson@gmail.com>
2019-05-20 18:59:18 +0000

    game initialisation tests added

--- assets/jasmine-tests/spec/main-spec.js
+++ assets/jasmine-tests/spec/main-spec.js
@@ -22,3 +22,36 @@ describe('Check javascript has loaded correctly',function(){
     
 })
 
+describe('Check game initialises correctly' , function() {
+    
+    beforeEach(() => {
+        setFixtures(`
+           <div class="modal fade" id="startModal" tabindex="-1" role="dialog" aria-labelledby="startModalTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="startModalCenterTitle">Welcome To Westeros</h5>
+                    <span aria-hidden="true">&times;</span>
+                </div>
+                <div class="modal-body">
+                    <form>
+                        <h3>What is your name traveller?</h3>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="name" placeholder="Name" aria-label="Name input" required>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-primary" data-dismiss="modal" id="start-btn">Start</button>
+                    <button type="button" class="btn btn-primary">Rules</button>
+                </div>
+            </div>
+        </div>
+    </div>
+        `)
+    });
+    
+    it('Modal should be loaded into the DOM' , function(){
+        expect($('#startModal')[0]).toBeInDOM()
+    })
+})
