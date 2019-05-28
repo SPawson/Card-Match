a4ef73d4ae39e78753560755fc13e3b2fdb203ef
Sam Pawson <samuel.pawson@gmail.com>
2019-05-28 19:06:44 +0000

    added player name functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,27 +34,28 @@ class AudioController {
         }
         fail = function() {
                 this.stopMusic();
-                this.failSound.play(); 
+                this.failSound.play();
         }
-        
+
         muteMusic = function() {
                 let soundBtn = $('#sound');
                 let stop = this.stopMusic()
                 let start = this.startMusic()
-                
+
                 soundBtn.on('click', () => {
-                        if(soundBtn.hasClass('fa-volume-up')){
+                        if (soundBtn.hasClass('fa-volume-up')) {
                                 soundBtn.removeClass('fa-volume-up');
                                 soundBtn.addClass('fa-volume-mute');
                                 this.stopMusic()
-                        } else{
+                        }
+                        else {
                                 soundBtn.removeClass('fa-volume-mute');
                                 soundBtn.addClass('fa-volume-up');
                                 this.startMusic()
                         }
                 });
 
-                
+
         }
 
 }
@@ -68,14 +69,14 @@ class ScoreController {
                 this.threeStar = 'Not a bad attempt'
                 this.fourStar = 'Well done, thats a great effort'
                 this.fiveStar = 'Excellent the knights of the realm will be thrilled with you!'
-                
+
                 this.scoreTitle = $('#scoreTitle');
                 this.scoreMessage = $('#endGameMessage')
         }
-       fail = function() {
-               this.scoreTitle.html(this.failTitle);
-               this.scoreMessage.html(this.failMessage);
-       } 
+        fail = function() {
+                this.scoreTitle.html(this.failTitle);
+                this.scoreMessage.html(this.failMessage);
+        }
 }
 
 class CardMatch {
@@ -111,14 +112,20 @@ class CardMatch {
                 })
         }
         
-        configureTimeOut = function(audio,shuffle,timeR,busy,countdown,countdownFunc){
-                 setTimeout(() => {
+        setName = function() {
+                var playerName = $('#name').val();
+                
+                $('#scoreName').html(playerName);
+        }
+
+        configureTimeOut = function(audio, shuffle, timeR, busy, countdown, countdownFunc) {
+                setTimeout(() => {
                         /*audio;
                         shuffle;
                         timeR = countdown;
                         busy = false;
                         countdown = countdownFunc;*/
-                        
+
                         this.audioController.startMusic();
                         this.shuffleCards(this.cardArray);
                         this.busy = false;
@@ -135,31 +142,32 @@ class CardMatch {
                 let countdown = this.countdown;
                 let countdownFunc = this.startCountdown();
                 */
-                
+
                 this.cardToBeChecked = null;
                 this.totalClicks = 0;
                 this.timeRemaining = this.totalTime;
                 this.matchedCards = [];
                 this.busy = true;
                 this.audioController.muteMusic();
-                
-                 setTimeout(() => {
+                this.setName();
+
+                setTimeout(() => {
                         /*audio;
                         shuffle;
                         timeR = countdown;
                         busy = false;
                         countdown = countdownFunc;*/
-                        
+
                         this.audioController.startMusic();
                         this.shuffleCards(this.cardArray);
                         this.busy = false;
                         this.countDown = this.startCountdown();
 
                 }, 500)
-               
+
                 this.timer = this.timer.text(this.timeRemaining)
                 this.turns = this.turns.text(this.totalClicks);
-                
+
                 this.hideCard();
 
         }
@@ -168,15 +176,15 @@ class CardMatch {
                 return setInterval(() => {
                         this.timeRemaining--;
                         this.timer = this.timer.text(this.timeRemaining);
-                        if(this.timeRemaining === 0) {
+                        if (this.timeRemaining === 0) {
                                 this.gameOver();
                         }
-                        
+
                 }, 1000)
-                
-                
+
+
         }
-        
+
         gameOver = function() {
                 clearInterval(this.countDown);
                 this.audioController.fail();
@@ -192,27 +200,27 @@ class CardMatch {
                         this.turns = this.turns.text(this.totalClicks);
                         $(card).addClass('visible');
                 }
-                
-                if(this.cardToBeChecked) {
+
+                if (this.cardToBeChecked) {
                         this.checkCardMatch(card);
                 }
-                else{
+                else {
                         this.cardToBeChecked = card;
                 }
         }
-        
+
         checkCardMatch = function(card) {
-                
-                if(this.getCardType(card) === this.getCardType(this.cardToBeChecked)){
-                //Match
+
+                if (this.getCardType(card) === this.getCardType(this.cardToBeChecked)) {
+                        //Match
                 }
-                
-                else{
+
+                else {
                         //mis Match
                 }
-        
+
         }
-        
+
         getCardType = function(card) {
                 let cardType = $('.card-front img').attr('src');
                 return cardType;
@@ -223,20 +231,26 @@ class CardMatch {
         }
 }
 
+
 $(document).ready(function() {
         $("#startModal").modal('show');
 
         let cards = $('.card');
-        let game = new CardMatch(5, cards);
+        let game = new CardMatch(100, cards);
         let start = $('#start-btn')
         let playAgain = $('#play-again')
 
+
+
         start.on('click', function() {
                 game.startGame();
+                console.log(name);
+
+                setName(name);
 
         });
-        
-         playAgain.on('click', function() {
+
+        playAgain.on('click', function() {
                 game.startGame();
 
         });
@@ -244,8 +258,8 @@ $(document).ready(function() {
         cards.on('click', function() {
                 game.cardFlip(this);
         });
-        
-     
+
+
 
 
 });
