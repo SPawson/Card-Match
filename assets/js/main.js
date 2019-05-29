145f18e9e5402f240d9d5944654049ae8c7d595c
Sam Pawson <samuel.pawson@gmail.com>
2019-05-29 15:29:18 +0000

    Basic game working functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,6 +64,7 @@ class ScoreController {
         constructor() {
                 this.failTitle = 'Fail';
                 this.failMessage = 'That was a disaster';
+                this.successTitle = 'Completed'
                 this.oneStar = 'You can do better than that';
                 this.twoStar = 'Keep trying';
                 this.threeStar = 'Not a bad attempt'
@@ -76,6 +77,31 @@ class ScoreController {
         fail = function() {
                 this.scoreTitle.html(this.failTitle);
                 this.scoreMessage.html(this.failMessage);
+        }
+
+        oneStarMessage = function() {
+                this.scoreTitle.html(this.successTitle);
+                this.scoreMessage.html(this.oneStar);
+        }
+
+        twoStarMessage = function() {
+                this.scoreTitle.html(this.successTitle);
+                this.scoreMessage.html(this.twoStar);
+        }
+
+        threeStarMessage = function() {
+                this.scoreTitle.html(this.successTitle);
+                this.scoreMessage.html(this.threeStar);
+        }
+
+        fourStarMessage = function() {
+                this.scoreTitle.html(this.successTitle);
+                this.scoreMessage.html(this.fourStar);
+        }
+
+        fiveStarMessage = function() {
+                this.scoreTitle.html(this.successTitle);
+                this.scoreMessage.html(this.fiveStar);
         }
 }
 
@@ -111,10 +137,10 @@ class CardMatch {
                         $(this).removeClass('visible')
                 })
         }
-        
+
         setName = function() {
                 var playerName = $('#name').val();
-                
+
                 $('#scoreName').html(playerName);
         }
 
@@ -192,6 +218,14 @@ class CardMatch {
                 $('#scoreModal').modal('show')
         }
 
+        victory = function() {
+                clearInterval(this.countDown);
+                this.audioController.success();
+                this.score.fiveStarMessage();
+                $('#scoreModal').modal('show')
+
+        }
+
 
         cardFlip = function(card) {
                 if (this.canCardFlip(card)) {
@@ -199,35 +233,97 @@ class CardMatch {
                         this.totalClicks++;
                         this.turns = this.turns.text(this.totalClicks);
                         $(card).addClass('visible');
+
+                        if (this.cardToBeChecked) {
+                                this.checkCardMatch(card);
+
+                        }
+                        else {
+                                this.cardToBeChecked = card;
+                        }
                 }
 
-                if (this.cardToBeChecked) {
-                        this.checkCardMatch(card);
-                }
-                else {
-                        this.cardToBeChecked = card;
-                }
+
         }
 
         checkCardMatch = function(card) {
 
-                if (this.getCardType(card) === this.getCardType(this.cardToBeChecked)) {
+                var currentCard = [this.getCardType(card), card];
+                var checkCard = [this.getCardType(this.cardToBeChecked), this.cardToBeChecked];
+
+                if (currentCard[0] === checkCard[0]) {
                         //Match
+                        this.cardMatched(currentCard, checkCard);
+                        this.cardToBeChecked = null
+
                 }
 
                 else {
                         //mis Match
+                        this.cardNotMatch(currentCard, checkCard);
+                        this.cardToBeChecked = null
+                }
+
+
+        }
+
+        cardNotMatch = function(card1, card2) {
+                this.busy = true;
+                console.log('Mismatch')
+
+
+
+                setTimeout(() => {
+
+                        $(card1[1]).removeClass('visible');
+                        $(card2[1]).removeClass('visible');
+
+                        this.busy = false;
+
+                }, 1000)
+        }
+
+
+        cardMatched = function(card1, card2) {
+                this.audioController.match();
+                this.matchedCards.push(card1[0]);
+                this.matchedCards.push(card2[0]);
+
+                /*setTimeout(() => {
+
+                        $(card1[1]).addClass('match');
+                        $(card2[1]).addClass('match');
+
+
+                        this.busy = false;
+
+                }, 1000)*/
+
+
+
+                console.log(this.cardMatched);
+
+                if (this.matchedCards.length === this.cardArray.length) {
+                        this.victory();
                 }
 
         }
 
         getCardType = function(card) {
-                let cardType = $('.card-front img').attr('src');
+                let cardType = card.getElementsByClassName('card-value')[0].src;
+                //$(card).add('.card-value').attr('src');
+                console.log(cardType)
                 return cardType;
         }
 
         canCardFlip = function(card) {
-                return true;
+                
+                let cardValue = this.getCardType(card);
+                if (!this.busy && !this.matchedCards.includes(cardValue) && card !== this.cardToBeChecked) {
+                        return true;
+
+                }
+
         }
 }
 
@@ -244,10 +340,6 @@ $(document).ready(function() {
 
         start.on('click', function() {
                 game.startGame();
-                console.log(name);
-
-                setName(name);
-
         });
 
         playAgain.on('click', function() {
